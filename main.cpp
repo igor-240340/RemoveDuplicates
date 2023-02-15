3878c3ae2ca4ee1a01ecb5fdfc5887fb59b27cf7
igor <igor.240340@gmail.com>
2023-02-16 06:06:52 +0700

    Init

---
+++ main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+    int s = 23; //
+    //char data[] = "AAA BBB AAA";
+    char data[] = "AAA BBB AAA cc def ddd"; // A B A c def d
+
+    int i = 1;
+    char* p = data + 1;
+    do {
+        char prev = *(p - 1);
+        char cur = *p;
+
+        if (cur == prev) {
+            memcpy(p - 1, p, s - i);
+        }
+        else
+            p++;
+
+        i++;
+    } while (*p != '\0');
+
+    std::cout << data << std::endl;
+}
